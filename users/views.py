854bf465c3a0abe9e9ff73902b8b0b1a41cbd3d8
Adi Gunawan Lase <adiglase6@gmail.com>
2023-06-05 20:55:18 +0700

    add current user detail api

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from users.serializers import CustomUserDetailsSerializer
@@ -8,6 +10,11 @@ UserModel = get_user_model()
 
 
 class UserView(ModelViewSet):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     queryset = UserModel.objects.all()
     serializer_class = CustomUserDetailsSerializer
+
+    @action(detail=False)
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
